9eb9f12b8c938138321023176899baae665f9684
Allan Pichardo <allan.pichardo@gmail.com>
2019-02-04 22:35:37 -0500

    added normalized close and volume to dataset

--- ml/yahoofinance.py
+++ ml/yahoofinance.py
@@ -1,5 +1,6 @@
 import matplotlib.dates as dates
 import pandas as pd
+import sklearn
 
 
 class PriceHistory:
@@ -7,9 +8,14 @@ class PriceHistory:
     def __init__(self, path):
         self._raw_df = pd.read_csv(path, index_col='Date', parse_dates=True, infer_datetime_format=True)
         self._raw_df.insert(loc=0, column='time', value=dates.date2num(self._raw_df.index.values))
+        self._raw_df['Norm Close'] = self.normalize(self._raw_df['Adj Close'])
+        self._raw_df['Volume'] = self.normalize(self._raw_df['Volume'])
 
     def get_dataframe(self):
         return self._raw_df
+
+    def normalize(self, column):
+        return (column - column.min()) / (column.max() - column.min())
 
     def get_trading_days(self):
         days = []
@@ -28,7 +34,11 @@ class TradingDay:
         self._close = raw_day['Close']
         self._adj_close = raw_day['Adj Close']
         self._volume = raw_day['Volume']
+        self._norm_close = raw_day['Norm Close']
         self._date = raw_day.name
+
+    def get_normalized_close(self):
+        return self._norm_close
 
     def get_open(self):
         return self._open
@@ -57,7 +67,9 @@ class TradingDay:
 
 class Candle:
 
-    def __init__(self, trading_day):
+    def __init__(self, trading_day: TradingDay):
+        self._close = trading_day.get_normalized_close()
+        self._volume = trading_day.get_volume()
         self._direction = 1 if trading_day.get_close() > trading_day.get_open() else -1
         self._direction = 0 if trading_day.get_close() == trading_day.get_open() else self._direction
         self._wick_up = (trading_day.get_high() - trading_day.get_close()) / trading_day.get_close()
@@ -75,3 +87,12 @@ class Candle:
 
     def get_size(self):
         return self._wick_down
+
+    def get_close(self):
+        return self._close
+
+    def get_volume(self):
+        return self._volume
+
+    def dataframe(self):
+        df = pd
